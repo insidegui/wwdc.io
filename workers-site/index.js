7a5819287562ab4dfe2f03e3647d156162845373
Guilherme Rambo <insidegui@gmail.com>
2020-07-18 17:44:08 -0300

    Moved AASA to worker script

--- workers-site/index.js
+++ workers-site/index.js
@@ -10,52 +10,79 @@ import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
 const DEBUG = false
 
 addEventListener('fetch', event => {
-  try {
-    event.respondWith(handleEvent(event))
-  } catch (e) {
-    if (DEBUG) {
-      return event.respondWith(
-        new Response(e.message || e.toString(), {
-          status: 500,
-        }),
-      )
+    try {
+        event.respondWith(handleEvent(event))
+    } catch (e) {
+        if (DEBUG) {
+            return event.respondWith(
+                new Response(e.message || e.toString(), {
+                    status: 500,
+                }),
+            )
+        }
+        event.respondWith(new Response('Internal Error', { status: 500 }))
     }
-    event.respondWith(new Response('Internal Error', { status: 500 }))
-  }
 })
 
+function aasaResponse() {
+    return {
+        "activitycontinuation": {
+            "apps": [
+                "8C7439RJLG.io.wwdc.app"
+            ]
+        },
+        "applinks": {
+            "apps": [],
+            "details": [{
+                "appID": "8C7439RJLG.io.wwdc.app",
+                "paths": [
+                    "NOT /",
+                    "*"
+                ]
+            }]
+        }
+    }
+}
+
 async function handleEvent(event) {
-  const url = new URL(event.request.url)
-  let options = {}
+    const url = new URL(event.request.url)
+    let options = {}
 
-  /**
-   * You can add custom logic to how we fetch your assets
-   * by configuring the function `mapRequestToAsset`
-   */
-  // options.mapRequestToAsset = handlePrefix(/^\/docs/)
+    /**
+     * You can add custom logic to how we fetch your assets
+     * by configuring the function `mapRequestToAsset`
+     */
+    // options.mapRequestToAsset = handlePrefix(/^\/docs/)
 
-  try {
-    if (DEBUG) {
-      // customize caching
-      options.cacheControl = {
-        bypassCache: true,
-      }
+    try {
+        if (DEBUG) {
+            // customize caching
+            options.cacheControl = {
+                bypassCache: true,
+            }
+        }
+
+        let url = new URL(event.request.url)
+
+        if (url.pathname == '/.well-known/apple-app-site-association') {
+            return new Response(JSON.stringify(aasaResponse()), { headers: { "Content-Type": "application/json" } })
+        }
+
+        return await getAssetFromKV(event, options)
+    } catch (e) {
+        // if an error is thrown try to serve the asset at 404.html
+        if (!DEBUG) {
+            try {
+                let notFoundResponse = await getAssetFromKV(event, {
+                    mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.html`, req),
+                })
+
+                return new Response(notFoundResponse.body, {...notFoundResponse, status: 404 })
+            } catch (e) {}
+        }
+
+        return new Response(e.message || e.toString(), { status: 500 })
     }
-    return await getAssetFromKV(event, options)
-  } catch (e) {
-    // if an error is thrown try to serve the asset at 404.html
-    if (!DEBUG) {
-      try {
-        let notFoundResponse = await getAssetFromKV(event, {
-          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.html`, req),
-        })
-
-        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
-      } catch (e) {}
-    }
-
-    return new Response(e.message || e.toString(), { status: 500 })
-  }
 }
 
 /**
@@ -66,15 +93,15 @@ async function handleEvent(event) {
  * to exist at a specific path.
  */
 function handlePrefix(prefix) {
-  return request => {
-    // compute the default (e.g. / -> index.html)
-    let defaultAssetKey = mapRequestToAsset(request)
-    let url = new URL(defaultAssetKey.url)
+    return request => {
+        // compute the default (e.g. / -> index.html)
+        let defaultAssetKey = mapRequestToAsset(request)
+        let url = new URL(defaultAssetKey.url)
 
-    // strip the prefix from the path for lookup
-    url.pathname = url.pathname.replace(prefix, '/')
+        // strip the prefix from the path for lookup
+        url.pathname = url.pathname.replace(prefix, '/')
 
-    // inherit all other props from the default request
-    return new Request(url.toString(), defaultAssetKey)
-  }
+        // inherit all other props from the default request
+        return new Request(url.toString(), defaultAssetKey)
+    }
 }
